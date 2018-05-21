cbd4885419b7d97a4253abd91b25ca8780e711c9
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-21 18:57:01 -0400

    sync

--- lib/prompt.zsh
+++ lib/prompt.zsh
@@ -22,16 +22,21 @@ p6dfz::prompt::init() {
 p6dfz::prompt::process() {
 
   setopt prompt_subst
-  PROMPT=
-  local line
-  for line in $PromptLines[@]; do
-#    echo "p:[p6df::prompt::$line::line]"
-    p6dfz::util::exists p6df::prompt::$line::line && cnt="\$(p6df::prompt::$line::line)" || cnt=$line
-    PROMPT="$PROMPT
-$cnt"
-  done
-  PROMPT="$PROMPT
+  PROMPT="\$(p6dfz::prompt::runtime $PromptLines[@])"
+  PROMPT="
+$PROMPT
 "
+}
+
+p6dfz::prompt::runtime() {
+
+  for line in $@; do
+    local func="p6df::prompt::$line::line"
+    if p6dfz::util::exists $func; then
+      local cnt=$($func)
+      [ -n "$cnt" ] && echo $cnt
+    fi
+  done
 }
 
 p6df::prompt::std::line() {
@@ -52,12 +57,42 @@ p6df::prompt::dir::line() {
   echo $dir
 }
 
+p6df::prompt::cloud::line() {
+
+  local -a clouds=(aws gcp azure) # salesforce digitialocean rspace)
+
+  local cloud
+  for cloud in $clouds[@]; do
+    p6dfz::util::exists p6df::prompt::${cloud}::line && p6df::prompt::${cloud}::line
+  done
+}
+
 p6df::prompt::lang::line() {
 
-  echo lang::all
+  local -a langs=(python perl ruby go java scala lua R)
+  local lang
+  local str=""
+  for lang in $langs[@]; do
+    local prefix=$(cmd_2_envprefix "$lang")
+    local func="p6df::prompt::${lang}::line"
+    local cntv=""
+    if p6dfz::util::exists $func; then
+      cntv=$($func)
+    else
+      cntv="n/a"
+    fi
+    str="$str $prefix:$cntv"
+  done
+  
+  str=${str## }
+  echo "lang:\t$str"
 }
 
 p6df::prompt::vc::line() {
 
-  p6dfz::util::exists p6df::prompt::git::line && p6df::prompt::git::line || echo vc::git
+  local -a vcs=(git svn hg lp p4 cvs mg)
+  local vc
+  for vc in $vcs[@]; do
+    p6dfz::util::exists p6df::prompt::${vc}::line && p6df::prompt::${vc}::line
+  done
 }
